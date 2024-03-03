69947729d3b673d24c6c10eb23e03e2b5b563300
emilee hatch <hatch.emilee.cm@gmail.com>
2024-03-03 01:46:01 -0700

    Modified the ContactDetailComponent, and updated css.

--- cms/src/app/contacts/contact-detail/contact-detail.component.ts
+++ cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-detail',
   templateUrl: './contact-detail.component.html',
   styleUrl: './contact-detail.component.css'
 })
-export class ContactDetailComponent {
 
+export class ContactDetailComponent {
+  // Define a variable of the Contact data type
+  public selectedContact: Contact;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // Initialize the variable with an example Contact or fetch it from a service
+    this.selectedContact = new Contact('1', 'John Doe', 'john.doe@example.com', '123-456-7890', '../../../assets/images/barzeer.jpg', []);
+  }
 }
