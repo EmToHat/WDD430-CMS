fa934ca030798773bc05d6058094375d6b5100e6
emilee hatch <hatch.emilee.cm@gmail.com>
2024-03-03 00:34:38 -0700

    Created the header component. And modified the header component.

--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactsComponent,
     ContactDetailComponent,
-    ContactListComponent
+    ContactListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
