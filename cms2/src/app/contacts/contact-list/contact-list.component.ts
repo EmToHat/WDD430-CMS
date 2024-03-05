b7d6a6e8b517ee45787aef38e23c442e791116e9
emilee hatch <hatch.emilee.cm@gmail.com>
2024-03-04 23:12:51 -0700

    Displayed the Contact List Component, added images, and updated css

--- cms2/src/app/contacts/contact-list/contact-list.component.ts
+++ cms2/src/app/contacts/contact-list/contact-list.component.ts
@@ -13,8 +13,8 @@ export class ContactListComponent {
   // Initialize the array with 2 dummy contact objects.
   constructor() {
     this.contacts = [
-      new Contact('1', 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', '../../assets/images/jacksonk.jpg'),
-      new Contact('2', 'Rex Barzee', 'barzeer@byui.edu', '208-496-3768', '../../assets/images/barzeer.jpg')
+      new Contact('1', 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', '../../../assets/images/jacksonk.jpg'),
+      new Contact('2', 'Rex Barzee', 'barzeer@byui.edu', '208-496-3768', '../../../assets/images/barzeer.jpg')
     ]
   }
 }
