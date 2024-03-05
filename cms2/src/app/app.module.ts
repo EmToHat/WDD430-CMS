e544b1c4dec6ffc1058e7b38d535e05d78ea59d1
emilee hatch <hatch.emilee.cm@gmail.com>
2024-03-04 21:44:44 -0700

    Created the main components, and the sub components of the contact component (contact-detail, and contact-list).

--- cms2/src/app/app.module.ts
+++ cms2/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { HeaderComponent } from './header/header.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    HeaderComponent,
+    DocumentsComponent,
+    MessagesComponent,
+    ContactListComponent,
+    ContactDetailComponent
   ],
   imports: [
     BrowserModule
