e2adb6bb13fddbc29485eec40fb1112f4f6ff962
emilee hatch <hatch.emilee.cm@gmail.com>
2024-03-04 23:05:44 -0700

    CSS Update to the contacts components

--- cms2/src/app/contacts/contact-list/contact-list.component.ts
+++ cms2/src/app/contacts/contact-list/contact-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contact } from '../contact-model';
 
 @Component({
   selector: 'cms2-contact-list',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './contact-list.component.css'
 })
 export class ContactListComponent {
+  // Define the variable to store the list of contacts.
+  public contacts: Contact[] = [];
 
+  // Initialize the array with 2 dummy contact objects.
+  constructor() {
+    this.contacts = [
+      new Contact('1', 'R. Kent Jackson', 'jacksonk@byui.edu', '208-496-3771', '../../assets/images/jacksonk.jpg'),
+      new Contact('2', 'Rex Barzee', 'barzeer@byui.edu', '208-496-3768', '../../assets/images/barzeer.jpg')
+    ]
+  }
 }
