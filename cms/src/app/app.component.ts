c07ef528ab3f9777a0207c99d4348cd0d0b5108e
emilee hatch <hatch.emilee.cm@gmail.com>
2024-03-18 19:34:04 -0600

    W06: Specified where the routed components are to be displayed

--- cms/src/app/app.component.ts
+++ cms/src/app/app.component.ts
@@ -7,13 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cms';
-
-  // Add a class variable named selectedFeature of the string datatype.
-  selectedFeature: string = 'messages';
-
-  // Method to switch the view based on the selected feature.
-  switchView(selectedFeature: string) {
-    // Assign the value of the selectedFeature input parameter to the class variable.
-    this.selectedFeature = selectedFeature;
-  }
 }
